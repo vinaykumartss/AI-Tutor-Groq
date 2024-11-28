37271949aed2451512a596c6082b54d5e980e0e6
Rohit Kumar <rkmahoba8@gmail.com>
2024-11-28 10:35:31 +0530

    minor-changes

--- app/api/routes.py
+++ app/api/routes.py
@@ -11,20 +11,22 @@ router = APIRouter()
 @router.post('/check-grammar', tags=["Grammar"])
 async def api_check_grammar(input_data: TextInput):
     correct_text = check_grammar(input_data.text)
+    appreciateText = None
     if input_data.text.lower().strip('?.') == correct_text.lower().strip('?.'):
-        return success_response(random.choice(appreciate_text))
-    return success_response(correct_text)
+        appreciateText = random.choice(appreciate_text)
+
+    return {"success": True, "appreciate_text": appreciateText, "text": input_data.text, "correct_text": correct_text}
 
 
 @router.post('/translation', tags=["Translator"])
 async def translation_text(input_data: TextInput):
     translated_text = translate_text(input_data.text)
-    return success_response(translated_text)
+    return {"success": True,"text": input_data.text, "data": translated_text}
 
 @router.post('/idiom', tags=["Idiom"])
 async def idiom_fun(input_data: TextInput):
     idiom_response_text = idiom_text(input_data.text)
-    return success_response(idiom_response_text)
+    return {"success": True,"text": input_data.text, "data": idiom_response_text}
 
 
 @router.post('/ai-tutor', tags=['AI-Tutor'])
