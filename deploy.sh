a8844078c1ba7e00852bd77f7e10595fe8889ddc
Nishit Goel <33784454+Nishit2408@users.noreply.github.com>
2025-09-21 12:39:50 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+git pull origin production 
+cd "$CUR_DIR"
+
+docker compose down --remove-orphans 
+docker images | grep ai-tutor-groq | awk '{print $3}' | xargs -r docker rmi -f
+docker compose up -d --build --no-cache
